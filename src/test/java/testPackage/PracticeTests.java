54dcedd8c9e164ceda9afd54f18119a9b8296aee
mohamedalshafey25 <52078877+mohamedalshafey25@users.noreply.github.com>
2025-06-28 12:22:26 +0300

    Trying the @Test annotation.

---
+++ src/test/java/testPackage/PracticeTests.java
@@ -0,0 +1,12 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+
+public class PracticeTests {
+
+    @Test
+    public void testMethod1() {
+        System.out.println("Test Method 1 is running");
+    }
+
+}
